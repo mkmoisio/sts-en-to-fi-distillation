ebd5fdbf22678c87be2f25313ba871ca2c058a65
Mikko Moisio <mmoisio@kiisseli.com>
2020-12-14 19:57:13 +0200

    reformat utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,30 +1,35 @@
-import os, sys
+import os
+import sys
 import requests
 from pathlib import Path
 import logging
 import logging.config
 
-path = Path(os.getcwd()) / 'config/logging.conf'
-logging.config.fileConfig(path)
+log_path = Path(os.getcwd()) / 'config/logging.conf'
+data_path = Path(os.getcwd()) / 'data'
+dl_url = 'https://nlpdistillation.blob.core.windows.net/nlpdistillation/'
+
+
+logging.config.fileConfig(log_path)
 logger = logging.getLogger('default')
 
 
-DL_URL = 'https://nlpdistillation.blob.core.windows.net/nlpdistillation/'
-OUTPUT_PATH = Path(os.getcwd()) / 'data'
 
 
-def download_data(file_name):
-    logger.info('Downloading file "' + file_name + '" from ' + DL_URL + file_name)
-    response = requests.get(DL_URL + file_name)
+def download_dataset(file_name):
+
+    logger.info('Downloading file "' + file_name +
+                '" from ' + dl_url + file_name)
+    response = requests.get(dl_url + file_name)
     if response.status_code == 200:
         response.encoding = 'utf-8'
-        logger.info('Saving file ' + file_name + 'from' + OUTPUT_PATH)
-        with open(OUTPUT_PATH / file_name, mode = 'w') as f:
+        logger.info('Saving file ' + file_name + ' to ' + str(data_path))
+        with open(data_path / file_name, mode='w') as f:
             f.write(response.text)
 
     else:
         logger.error('Failed to download file ' + file_name)
 
-download_data('train-set.tsv')
 
-
+if __name__ == "__main__":
+    download_data(sys.argv[1])

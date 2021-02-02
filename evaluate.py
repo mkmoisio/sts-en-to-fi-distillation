821a500d6e38aed63a4d19a9c82f1649fafedcc5
Mikko Moisio <mmoisio@kiisseli.com>
2021-02-02 19:34:23 +0200

    evaluation, cont

--- evaluate.py
+++ evaluate.py
@@ -3,11 +3,15 @@ import logging
 import logging.config
 import pandas as pd
 import os
+from sentence_transformers import SentenceTransformer, SentencesDataset, InputExample, losses, LoggingHandler, models
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+
 
 log_path = Path(os.getcwd()) / 'config/logging.conf'
 data_path = Path(os.getcwd()) / 'data'
-output_path = Path(os.getcwd()) / 'out'
+output_path = Path(os.getcwd()) / 'results'
+model_path = Path(os.getcwd()) / 'models'
+
 logging.config.fileConfig(log_path)
 logger = logging.getLogger('default')
 
@@ -16,11 +20,42 @@ logger = logging.getLogger('default')
 
 if __name__ == "__main__":
     df = pd.read_csv(data_path / 'STS-en-en-fi-fi.tsv', sep = '\t')
-    print(df.head())
+    #print(df.head())
+    
+
+    # Evaluators for different STS tasks
     scores = df['score'].tolist()
     evaluators = []
-    evaluators.append(EmbeddingSimilarityEvaluator(df['fi1'].tolist(), df['fi2'].tolist(), scores))
+    evaluators.append(EmbeddingSimilarityEvaluator(df['fi1'].tolist(), df['fi2'].tolist(), scores, name = 'FI-FI'))
+    evaluators.append(EmbeddingSimilarityEvaluator(df['en1'].tolist(), df['en2'], scores, name='EN-EN'))
+    evaluators.append(EmbeddingSimilarityEvaluator(df['en1'].tolist(), df['fi2'], scores, name='EN-FI'))
+
+    # Finnish baselines 
+
+    word_embedding_model = models.Transformer('TurkuNLP/bert-base-finnish-cased-v1', max_seq_length=128)
+    dim = word_embedding_model.get_word_embedding_dimension()
+    mean_pooling = models.Pooling(dim, pooling_mode_mean_tokens=True)
+    max_pooling = models.Pooling(dim, pooling_mode_max_tokens=True, pooling_mode_mean_tokens=False)
+    cls_pooling = models.Pooling(dim, pooling_mode_cls_token=True, pooling_mode_mean_tokens=False)
     
+    loaded_models = {}
+
+    loaded_models['FinBERT-MEAN'] =  SentenceTransformer(modules=[word_embedding_model, mean_pooling])
+    loaded_models['FinBERT-MAX'] = SentenceTransformer(modules=[word_embedding_model, max_pooling])
+    loaded_models['FinBERT-CLS'] = SentenceTransformer(modules=[word_embedding_model, cls_pooling])
+
+
+    # Trained models
+
+    #loaded_models['Extracted'] = SentenceTransformer(model_path + '/')
+    #loaded_models['Inter-lingual'] = SentenceTransformer(model_path + '/XLM-R-single-distil-3')
+    #loaded_models['Cross-lingual'] = SentenceTransformer(model_path + '/XLM-R-distilled-3')
+
+    for model_name, model in loaded_models.items():
+        logger.info('Evaluating model: ' + model_name)
+        for evaluator in evaluators:
+            model.evaluate(evaluator)
+
 
 
 

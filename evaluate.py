ad56204228506e6d4a172f9fdaa30341e2aac44b
mikko <mmoisio@kiisseli.com>
2021-02-19 20:32:24 +0200

    save results to file, generate barcharts

--- evaluate.py
+++ evaluate.py
@@ -2,10 +2,14 @@ from pathlib import Path
 import logging
 import logging.config
 import pandas as pd
+import numpy as np
 import os
 from sentence_transformers import SentenceTransformer, SentencesDataset, InputExample, losses, LoggingHandler, models
-from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator, SimilarityFunction
+import simplejson as json
 
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 log_path = Path(os.getcwd()) / 'config/logging.conf'
 data_path = Path(os.getcwd()) / 'data'
@@ -25,9 +29,9 @@ if __name__ == "__main__":
     # Evaluators for different STS tasks
     scores = df['score']
     evaluators = []
-    evaluators.append(EmbeddingSimilarityEvaluator(df['fi1'], df['fi2'], scores, name='FI-FI'))
-    evaluators.append(EmbeddingSimilarityEvaluator(df['en1'], df['en2'], scores, name='EN-EN'))
-    evaluators.append(EmbeddingSimilarityEvaluator(df['en1'], df['fi2'], scores, name='EN-FI'))
+    evaluators.append(EmbeddingSimilarityEvaluator(df['fi1'], df['fi2'], scores, name='FI-FI', main_similarity = SimilarityFunction.COSINE))
+    evaluators.append(EmbeddingSimilarityEvaluator(df['en1'], df['en2'], scores, name='EN-EN', main_similarity = SimilarityFunction.COSINE))
+    evaluators.append(EmbeddingSimilarityEvaluator(df['en1'], df['fi2'], scores, name='EN-FI', main_similarity = SimilarityFunction.COSINE))
 
     # Finnish baselines, https://huggingface.co/TurkuNLP/bert-base-finnish-cased-v1
     
@@ -48,14 +52,32 @@ if __name__ == "__main__":
     #loaded_models['Extracted'] = SentenceTransformer(model_path + '/')
     #loaded_models['Inter-lingual'] = SentenceTransformer(model_path + '/XLM-R-single-distil-3')
     #loaded_models['Cross-lingual'] = SentenceTransformer(model_path + '/XLM-R-distilled-3')
+
+    results = {}
     for evaluator in evaluators:
         logger.info('Evaluating task: ' + evaluator.name)
+        results[evaluator.name] = {}
         for model_name, model in loaded_models.items():
             logger.info('Evaluating model: ' + model_name)
-            model.evaluate(evaluator, output_path)
+            spearman_cosine = model.evaluate(evaluator)
+            results[evaluator.name][model_name] = spearman_cosine 
+    with open(output_path / 'results.json', 'w') as fp:        
+        json.dump(results, fp)
 
 
+    colors = mcolors.TABLEAU_COLORS
+    for item in results.items():
+        task = item[0]
+        labels = item[1].keys()
+        scores = item[1].values()
+        fig, ax1 = plt.subplots(figsize=(8, 6))
 
+        index = np.arange(len(scores))
+        plt.xticks(index, labels)
+        plt.ylim([0.0, 1.0])
+        plt.ylabel('Spearman\'s ρ,' + task)
+        plt.bar(index, scores, color = colors)
+        plt.savefig(str(output_path) +'/' + task + '.png')
 
 
 
